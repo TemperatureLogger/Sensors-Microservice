cf42991134afb72b3d8c775deabe843d9829fd39
Iulian <iulianolaru249@yahoo.com>
2021-05-31 09:12:15 +0300

    Refision

--- data_generator.py
+++ data_generator.py
@@ -6,11 +6,14 @@ import sys
 import requests
 import math
 
+#Unique Device Serial Number
+DEVICE_SERIAL = 111111
+
 #Middle server URL
 url = 'http://157.245.65.94:3000/api/measurements'
 
 #Probing frequency
-probe_freq = 1#int(sys.argv[1])
+probe_freq = 2
 
 #Sensor temperature reads limits
 MIN_TEMP_CELSIUS = 0.0
@@ -21,32 +24,32 @@ MIN_HUMIDITY_PERCENT = 20.0
 MAX_HUMIDITY_PERCENT = 90.0
 
 #Hardware specification
-sensor = Adafruit_DHT.DHT11
-DHT11_pin = 23
+sensor = Adafruit_DHT.DHT22
+DHT_PIN = 4
 
 #Data
 entries = []
-#{
-#    "temperature":"1112.1312321",
-#    "humidity": "7811111.123213"
-#}
 
-
-#GENERATE DUMMY DATA
 while True:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT_PIN)
+    # if humidity is not None and temperature is not None:
+    #     print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    # else:
+    #     print("Failed to retrieve data from humidity sensor")
+
     #init new dictionary
     entry = {}
 
     #get dummy read
-    # tstamp = time.strftime("%Y-%d-%b %H-%M-%S")
     tstamp = math.trunc(time.time())
     temperature = random.uniform(MIN_TEMP_CELSIUS, MAX_TEMP_CELSIUS)
     humidity = random.uniform(MIN_HUMIDITY_PERCENT, MAX_HUMIDITY_PERCENT)
 
     #add to dictionary
-    entry['tstamp'] = tstamp
+    entry['time'] = tstamp
     entry['temperature'] = temperature
     entry['humidity'] = humidity
+    entry['serialNumber'] = DEVICE_SERIAL
 
     print(entry)
     # Send generated data to middle server
@@ -57,12 +60,3 @@ while True:
 
     # wait desired amount of time
     time.sleep(probe_freq)
-
-#UNCOMMENT WHEN SENSOR IS REPLACED
-
-# humidity, temperature = Adafruit_DHT.read(sensor, DHT11_pin)
-# 
-# if humidity is not None and temperature is not None:
-    # print('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-# else:
-    # print('Failed to get reading from the sensor. Try again!')
